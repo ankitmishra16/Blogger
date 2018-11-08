cb87d435e237a0cdfbd8ba3938826b9b00136977
Vivek Patare <vivek.patare@students.iiit.ac.in>
2018-11-09 04:10:00 +0530

    home page updated(FIx needed)

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -19,7 +19,7 @@ def welcome():
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    return render_template('home2.html', posts=posts)
 
 
 @app.route("/about")
---
+++ flaskblog/static/js/grid.js
@@ -0,0 +1,13 @@
+var COL_COUNT = 3; // set this to however many columns you want
+var container = document.getElementById('block-contain'),
+    col_heights = [];
+for (var i = 0; i <= COL_COUNT; i++) {
+  col_heights.push(0);
+}
+for (var i = 0; i < container.children.length; i++) {
+  var order = (i + 1) % COL_COUNT || COL_COUNT;
+  container.children[i].style.order = order;
+  col_heights[order] += parseFloat(container.children[i].style.height);
+}
+var highest = Math.max.apply(Math, col_heights);
+container.style.height = highest+'px';
